85a098b31acc587cf7ef2bc13b051442a71b6dba
jackjj1125 <77815395+jackjj1125@users.noreply.github.com>
2021-12-07 15:38:05 -0700

    Update mainwindow.h
    added enemies

--- mainwindow.h
+++ mainwindow.h
@@ -5,47 +5,32 @@
 #include <QGraphicsScene>
 #include <QGraphicsView>
 #include <vector>
-
 #include "game.h"
 
+QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
-
+QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    // constructor and destructor
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-    void createGameGrid(QColor color);
+    void createGameGrid();
 
+    void makeCastle(QColor color);
+    void makeBridge(QColor color);
+    void makeRiver();
+    void makeTree(int i, int j);
+    void makeRock(int i, int j);
+    void makeEnemy(int i, int j);
 
-    // methods for preforming turn/step of game
-    void turnCount(); //handles turns
-    void checkAlive(); //checks if cells should be alive or dead
-
-    // getters and setters for cell neighbors
-    int checkNeighbors(std::vector<game*> neighbors); //checks neighbors
-    std::vector<game*> getNeighbors(int i, int j); // gets cell neighbors
-
-    bool is_alive(); //checks if a given cell is alive
-
-    // increase and decrease population counter
-    void increasePopulation();
-    void decreasePopulation();
-
-    void neighborSlot(int x, int y);
-
-    int population_; // population counter
-
-    // timer
-
-    void changeInterval(int t);
-
-    void updateGraph();
+    void setPlayer(int x, int y, QColor color);
+    void updatePlayerPositionToFalse(int x, int y, QColor color);
+    void movePlayer(int option);
 
 
 private slots: // slots for buttons on UI
@@ -53,43 +38,37 @@ private slots: // slots for buttons on UI
     void on_playButton_click(); //slot for play game button
     void on_restartButton_click(); // restarts game (One of our additional features)
 
-    void clickCellSlot(game * cell); // slot for mouse press events (clicking cells on UI)
+    //Buttons for movement
+    void up_button_clicked();
 
+    void down_button_clicked();
 
-        //Buttons for movement
-    void on_toolButton_clicked();
+    void left_button_clicked();
 
-    void on_toolButton_2_clicked();
-
-    void on_toolButton_4_clicked();
-
-    void on_toolButton_3_clicked();
+    void right_button_clicked();
 
 private:
     Ui::MainWindow *ui;
     QGraphicsScene *scene;
-
     QGraphicsScene *MakeBoard_;
-    QGraphicsScene *MakePopGraph_;
 
     int cell_height_;
     int cell_width_;
-    int turnCounter_ = 0;
-    QTimer *timer;
 
-    game * cells[10][10];
-    std::vector <Bar*> popBar_;
-    Bar * prev_bar_;
+    int player_health_;
+    int player_score_;
+    int player_attack_;
 
-    // bar graph fields
-    int y_bar;
-    double h_bar;
-    int bar_height_;
-    int bar_width_;
+    game * cells[10][20];
 
-    // start flag for timer
-    bool start_;
+    Player *p1_;
+    Player *p2_;
 
-    QColor newGameColor;
+    QColor newGameColor_;
+    QColor player_color_;
+    QColor river_color_;
+    QColor rock_color_;
+    QColor tree_color_;
+    QColor enemy_color_;
 };
 #endif // MAINWINDOW_H
